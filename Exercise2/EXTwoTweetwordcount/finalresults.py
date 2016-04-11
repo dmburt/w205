948d55b0b689b3e2e91d46fd4dc4f437c74c9496
dmburt <dmburt@berkeley.edu>
2016-04-11 00:48:25 -0400

    Update finalresults.py

--- Exercise2/EXTwoTweetwordcount/finalresults.py
+++ Exercise2/EXTwoTweetwordcount/finalresults.py
@@ -6,14 +6,35 @@ import getopt
 
 def main(argv):
 
+    conn=psycopg2.connect(database='tcount', user="postgres", password="pass", host="localhost", port="5432")
+    cur=conn.cursor()
+
+
     try:
         opts, args = getopt.getopt(argv,"hw",["help", "word="])
 
     except getopt.GetoptError:
-        print "Usage: finalresults.py -w <word>"
         sys.exit(2)
 
+    if len(argv) == 0:
+        sql = "SELECT word, n FROM tweetwordcount ORDER BY word ASC;"
+        cur.execute(sql)
+
+        rows = cur.fetchall()
+        for row in rows:
+            w = row[0]
+            n = int(row[1])
+            print "Word: '%s': %d"%(w, n)
+
+        conn.commit()
+        conn.close()
+
+        if len(rows) == 0:
+            print "No words in feed."
+        return
+
     for opt, arg in opts:
+
         if opt == "-h":
             print "Usage: finalresults.py -w <word>"
 
@@ -41,3 +62,4 @@ def main(argv):
 
 if __name__ == "__main__":
     main(sys.argv[1:])
+
